6fd353d6131d87a83eeaa1ab7d69000ef11e975a
hashimmalik59 <hm3630450@gmail.com>
2024-09-01 11:02:52 +0500

    hide navigation

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 let navBar = document.querySelector("span");
+navBar.style.display = "none";
 
 let email = prompt("Enter your Email");
 let password = prompt("Enter your Password");
 
 if (email === "hm3630450@gmail.com" && password === "pak...321") {
+  navBar.style.display = "block";
   navBar.innerHTML = "Home     About     Contact     Portfolio";
   alert("Congratulation!");
 } else {
   alert("Your Email and Password are Incorrect");
+  navBar.innerHTML = "";
 }
